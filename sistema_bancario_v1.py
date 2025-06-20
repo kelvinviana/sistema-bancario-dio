418efd0a0151020ff02c0497a0b206de5bd5f0e4
kelvinviana <kelvin.22viana@gmail.com>
2025-06-20 07:42:12 -0300

    Atualização de código.

--- sistema_bancario_v1.py
+++ sistema_bancario_v1.py
@@ -19,9 +19,14 @@ while True:
 
     if opcao == "1":
         valor_deposito = float(input("Digite o valor que deseja depositar: "))
-        saldo += valor_deposito
-        extrato += f"Depósito de R$ {valor_deposito:.2f}\n"
-        print("Valor depositado com sucesso!\n\nO que deseja fazer a seguir?")
+
+        if valor_deposito <= 0:
+            print("\nValor inválido!\nDigite um valor maior que 0 (zero)\nTente novamente.")
+
+        else:
+            saldo += valor_deposito
+            extrato += f"Depósito de R$ {valor_deposito:.2f}\n"
+            print(f"\nValor depositado com sucesso!\nSaldo atual: R$ {saldo:.2f}\nO que deseja fazer a seguir?")
     
     elif opcao == "2":
         if numero_saques < 3:
@@ -29,21 +34,25 @@ while True:
             print(f"Quantidade de saques restantes permitidos no dia: {LIMITE_SAQUES - numero_saques}\n")
             valor_sacado = float(input("Digite o valor que deseja sacar: "))
 
-            if valor_sacado > 500:
-                print(f"O valor solicitado ultrapassa o limite diário permitido de R$ {limite:.2f}\nTente novamente.")
-
-            elif valor_sacado > 0 and valor_sacado <= 500:
-                saldo -= valor_sacado
-                extrato += f"Saque de R$ {valor_sacado:.2f}\n"
-                numero_saques += 1
-                print("Valor sacado com sucesso!\n")
-
+            if saldo < valor_sacado:
+                print ("\nVocê não possui saldo suficiente!\nConsulte seu extrato para maiores informações.\nTente novamente.")
+            
             else:
-                print("Valor inválido!\nTente novamente.")
+                if valor_sacado > 500:
+                    print(f"\nO valor solicitado ultrapassa o limite diário permitido de R$ {limite:.2f}\nTente novamente.")
 
-            print("\nO que deseja fazer a seguir?")
+                elif valor_sacado > 0 and valor_sacado <= 500:
+                    saldo -= valor_sacado
+                    extrato += f"Saque de R$ {valor_sacado:.2f}\n"
+                    numero_saques += 1
+                    print(f"\nValor sacado com sucesso!\nSaldo atual: R$ {saldo:.2f}")
+
+                else:
+                    print("\nValor inválido!\nTente novamente.")
+
+                print("\nO que deseja fazer a seguir?")
         else:
-            print(f"LIMITE DIÁRIO DE SAQUE ATINGIDO!\nREALIZE ESTA OPERAÇÃO NOVAMENTE AMANHÃ!\n\nO que deseja fazer a seguir?")
+            print(f"\nLIMITE DIÁRIO DE SAQUE ATINGIDO!\nREALIZE ESTA OPERAÇÃO NOVAMENTE AMANHÃ!\n\nO que deseja fazer a seguir?")
     
     elif opcao == "3":
         print("Histórico de Movimentações")
